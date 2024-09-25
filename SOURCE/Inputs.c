ea8d631b19c82ef5a9eb678afbd893cf11633aae
STLarus <teo.vuckovic@gmail.com>
2024-09-25 22:37:25 +0200

    ne prolazi linker, ne može naći pthread

---
+++ SOURCE/Inputs.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <unistd.h>
+
+#define NUM_INPUTS 6
+#define POLL_INTERVAL_MS 20
+
+int inputPins[NUM_INPUTS] = { 4, 17, 27, 22, 5, 26 }; // GPIO pinovi za ulaze
+volatile int impulseCount[NUM_INPUTS] = { 0 }; // Brojači impulsa
+
+void input_setup() {
+    wiringPiSetup();  // Inicijalizuj WiringPi
+
+    // Postavi pinove kao ulaze
+    for (int i = 0; i < NUM_INPUTS; i++) {
+        pinMode(inputPins[i], INPUT);
+    }
+}
+
+void input_loop() {
+    int previousState[NUM_INPUTS] = { 0 };
+
+    while (1) {
+        for (int i = 0; i < NUM_INPUTS; i++) {
+            int currentState = digitalRead(inputPins[i]);
+
+            // Proveri da li je došlo do promene (npr. opadajući signal)
+            if (currentState == LOW && previousState[i] == HIGH) {
+                impulseCount[i]++;
+            }
+
+            previousState[i] = currentState; // Ažuriraj prethodno stanje
+        }
+
+        // Ispiši trenutne brojače
+        printf("Brojači impulsa:\n");
+        for (int i = 0; i < NUM_INPUTS; i++) {
+            printf("Pin %d: %d\n", inputPins[i], impulseCount[i]);
+        }
+        printf("\n");
+
+        usleep(POLL_INTERVAL_MS * 1000); // Pauza od 20 ms
+    }
+}
---
+++ SOURCE/Wiegand.c
@@ -0,0 +1,66 @@
+﻿#include <stdlib.h>
+#include <wiringPi.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define D0_PIN 13  // GPIO pin za D0
+#define D1_PIN 6  // GPIO pin za D1
+#define TIMEOUT_MS 50 // Timeout u milisekundama
+
+volatile int bitCount = 0;
+volatile unsigned long data = 0;
+volatile bool timeout = false;
+
+// Funkcija za upravljanje timeout-om
+void* timeoutHandler(void* arg) {
+    usleep(TIMEOUT_MS * 1000); // Pretvori u mikrosekunde
+    timeout = true;
+    return NULL;
+}
+
+void d0Interrupt(void) {
+    data = (data << 1);  // pomeri levo i dodaj 0
+    bitCount++;
+    timeout = false;  // Resetuj timeout
+}
+
+void d1Interrupt(void) {
+    data = (data << 1) | 1;  // pomeri levo i dodaj 1
+    bitCount++;
+    timeout = false;  // Resetuj timeout
+}
+
+void wiegand_setup() {
+    wiringPiSetup();  // inicijalizuj wiringPi
+
+    pinMode(D0_PIN, INPUT);  // postavi D0 kao ulaz
+    pinMode(D1_PIN, INPUT);  // postavi D1 kao ulaz
+
+    // Postavi prekidače
+    wiringPiISR(D0_PIN, INT_EDGE_FALLING, &d0Interrupt);
+    wiringPiISR(D1_PIN, INT_EDGE_FALLING, &d1Interrupt);
+}
+
+void wiegand() {
+    while (1) {
+        if (timeout) {
+            // Ako je timeout istekao, obradi primljene podatke
+            if (bitCount > 0) {
+                printf("Primljen Wiegand signal: %lu, broj bitova: %d\n", data, bitCount);
+                // Resetuj podatke
+                data = 0;
+                bitCount = 0;
+            }
+        }
+        else {
+            // Pokreni novi tajmer svaki put kada se detektuje impuls
+            pthread_t timerThread;
+            timeout = false; // Postavi timeout na false
+            pthread_create(&timerThread, NULL, timeoutHandler, NULL);
+            pthread_detach(timerThread); // Odvojimo thread
+            usleep(1000); // Kratka pauza da ne saturiramo CPU
+        }
+    }
+}
